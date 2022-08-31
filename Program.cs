138ac116aca0130d25b23e5f7915be546b33eddb
Levi-Marinho <102664417+Levi-Marinho@users.noreply.github.com>
2022-08-31 16:18:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int x = 1;
+int y = 2;
+int z = 1;
+
+Console.WriteLine(x == y);    // False
+Console.WriteLine(x != y);    // True
+Console.WriteLine(x == z);    // True
+
+Console.WriteLine(x < y);    // True
+Console.WriteLine(x >= z);    // True
